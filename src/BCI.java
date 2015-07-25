0409b4208e71d17c7364834ee0434921691b873f
Neural Nomad III <neural.nomad.III@gmail.com>
2015-07-25 16:06:17 -0400

    Will now check the number of records (cards) in the DB and upthat on the main screen.

--- src/BCI.java
+++ src/BCI.java
@@ -5,8 +5,14 @@ import javax.swing.JButton;
 
 
 
+import javax.swing.JOptionPane;
+
 import java.sql.*;
 import java.util.Collection;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 
 public class BCI {
@@ -37,7 +43,7 @@ public class BCI {
 	
 	public BCI() {
 		initialize();
-		connection = SqliteConnection.dbConnector();
+		
 		
 		
 		
@@ -64,5 +70,45 @@ public class BCI {
 		JButton btnRemoveCard = new JButton("Remove a Card");
 		btnRemoveCard.setBounds(638, 277, 187, 62);
 		frame.getContentPane().add(btnRemoveCard);
+		
+		JLabel lblNewLabel = new JLabel("Total cards in DB");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(50, 289, 200, 50);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblcardCount = new JLabel("");
+		lblcardCount.setHorizontalAlignment(SwingConstants.CENTER);
+		lblcardCount.setBounds(293, 289, 200, 50);
+		frame.getContentPane().add(lblcardCount);
+		connection = SqliteConnection.dbConnector();
+		
+		try 
+		{
+				String querry = "select count(*) from cards"; // use sql to get a count of the total record for table cards
+				PreparedStatement pst = connection.prepareStatement(querry); //assign the prepared statement
+				ResultSet rst =pst.executeQuery(); //execute with extreme prejudice
+				int records = 0;
+				while(rst.next()) // loop through records returned
+				{
+					records = rst.getInt(1); // get the count which is a int from record 1 and assign it to records
+					
+					
+				}
+				
+				
+				
+				
+				lblcardCount.setText(String.valueOf(records)); //update the screen
+				
+
+				
+				rst.close();
+				pst.close();
+		}
+			catch(Exception e)
+			{
+				JOptionPane.showMessageDialog(null, "Get number of cards failed..." + e);
+			}
+		
 	}
 }
